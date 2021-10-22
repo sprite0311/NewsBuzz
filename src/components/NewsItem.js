0f369841f4b3e2dc00dc6bb1e92c3a4eef690073
Sarthak Dubey <dubey.sarthak0311@gmail.com>
2021-10-22 16:53:16 +0530

    yo

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-  
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
-    
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+
     return (
       <div className="my-3">
-        <div className="card" >
-          <img src={!imageUrl?"https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png":imageUrl} className="card-img-top" alt="" />
+        <div className="card">
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex: '1'}}>
+            {source}
+          </span>
+          <img
+            src={
+              !imageUrl
+                ? "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt=""
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}... </h5>
+            <p className="card-text">{description}...</p>
             <p className="card-text">
-              {description}...
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
             </p>
-            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
               Read more
             </a>
           </div>
